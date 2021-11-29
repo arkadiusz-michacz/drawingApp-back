0a5daa0ff61745cddb04406339c8cbaf8aab9be6
Arkadiusz Michacz <arkadiusz.michacz@gmail.com>
2021-11-29 22:18:33 +0100

    added initial emit on new client joining

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { Console } = require('console');
 const app = require('express');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http,{
@@ -9,15 +10,22 @@ const io = require('socket.io')(http,{
       }
 });
 
-
+let newestData;
 
 io.on('connection', (socket)=>{
     console.log('User online');
+    if(newestData !== undefined){
+        console.log("Newestdata exists");
+        socket.emit('canvas-data',newestData);
+    }
 
     socket.on('canvas-data',(data)=>{
         socket.broadcast.emit('canvas-data',data);
+        newestData = data;
     })
 })
+
+
 
 const server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 http.listen(server_port,()=>{
